8c86256069409467d1c7bc5021a8053b143be7df
muratbabayiğit <babayigit@gmail.com>
2026-06-05 21:50:13 +0300

    proje v1.4

--- src/test/java/tests/Jenkins.java
+++ src/test/java/tests/Jenkins.java
@@ -69,6 +69,7 @@ public class Jenkins {
         System.out.println("deneme");
         System.out.println("deneme");
         System.out.println("deneme");
+        System.out.println("deneme");
     }
 
 
